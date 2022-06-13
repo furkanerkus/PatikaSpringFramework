35ef4680aa67253c0fa9187f0e7f7dd1622491e9
Furkan Erkus <erkusfrkn@gmail.com>
2022-06-13 14:14:21 +0300

    StereoType

---
+++ src/main/java/com/furkan/springframework/stereotype/StereoTypeBean.java
@@ -0,0 +1,16 @@
+package com.furkan.springframework.stereotype;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@BenimStereoType
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StereoTypeBean {
+
+    private Long id;
+    private String name = "Merhabalar";
+    private String trade;
+}
